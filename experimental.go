bac7e6f30019eefc3ba873ccec539c2b7a72cf34
vadgun <vadgun@hotmail.com>
2021-04-19 06:51:05 -0500

    puerto

--- experimental.go
+++ experimental.go
@@ -84,5 +84,5 @@ func main() {
 	app.Post("/obtenconfig", calificacionescontroller.ObtenConfig)
 	app.Post("/guardaconfiguracion", calificacionescontroller.GuardaConfiguracion)
 
-	app.Run(iris.Addr(":8080"))
+	app.Run(iris.Addr(":80"))
 }
